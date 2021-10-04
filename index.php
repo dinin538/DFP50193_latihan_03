b415c9b5b76f42a3da4d1f1301fe0193834b312b
dinin538 <dnnblh@gmail.com>
2021-10-04 11:58:50 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 03</title>
+</head>
+<body>
+    <h1>Asas PHP</h1>
+
+    <?php 
+    // komen satu paris
+    #komen satu baris
+    /* 
+    komen bnyk baris 
+    */
+    $namaPelajar = 'Dini Nabilah';
+    $umur = '20';
+    echo "<h2>Hello $namaPelajar umur $umur tahun</h2>"; #output
+
+    $noKp = '010722060136';
+    ?>
+</body>
+</html>
